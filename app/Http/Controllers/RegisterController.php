fd56205f5ee917059eb8180ddb2dfea05964b593
Sana <sanamgh138@gmail.com>
2025-07-17 15:48:34 +0330

    datetime task

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,6 +6,7 @@ use App\Exceptions\InvalidNameException;
 use App\Exceptions\NotFoundException;
 use App\Http\Requests\Auth\RegisterPostRequest;
 use App\Models\User;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -17,6 +18,17 @@ class RegisterController extends Controller
 
     public function index()
     {
+//        $datetime2 = Carbon::parse('2025-09-09 15:15');
+//        $datetime = Carbon::now();
+//        $datetime3 = $datetime->toDateString();
+//        $add = $datetime2->addWeeks(2)->subDays(3);
+//        $month = $datetime->month;
+//        $firstWeek = $datetime2->startOfWeek();
+//        $isBefore = $datetime2 ->isAfter($datetime);
+//        $humanise = $datetime->diffForHumans($datetime2);
+//        $subWeek  = $datetime->diffInWeeks($datetime2);
+//        $formatted = $datetime->format('Y-m-d H:i');
+//        dd($month);
 //        dd(trimUp('sana'));
 //        try {
 //            $this->test(11);
